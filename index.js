435e091726eca7e13d131abcc8817de225a51cbf
dre7djib <djibril.camara.pradier@gmail.com>
2023-04-21 18:10:58 +0200

    Win Commit

--- index.js
+++ index.js
@@ -39,7 +39,25 @@ class Player {
     }
 }
 
+class Platform {
+    constructor( {x , y}) {
+        this.position = {
+            y,
+            x 
+        }
+        this.width = 200
+        this.height = 20
+    }
+
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
 const player = new Player()
+const platforms = [new Platform({x: 200, y:800}), new Platform({x:300, y: 700})]
+
 const keys = {
     right: {
         pressed: false
@@ -49,16 +67,52 @@ const keys = {
     }
 }
 
+
+let scrollOffset = 0
+
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0,0, canvas.width, canvas.height)
     player.update()
+    platforms.forEach(platform => {
+        platform.draw()
+    })
 
-    if (keys.right.pressed == true) {
+    if (keys.right.pressed == true && player.position.x < 400 ) {
         player.velocity.x = 5
-    } else if (keys.left.pressed == true) {
+    } else if (keys.left.pressed == true && player.position.x > 70) {
         player.velocity.x = -5
-    } else player.velocity.x = 0
+    } else {
+        player.velocity.x = 0
+        if (keys.right.pressed){
+            scrollOffset += 5
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+            } )
+        } else if (keys.left.pressed) {
+            scrollOffset -= 5
+            platforms.forEach(platform => {
+                platform.position.x += 5
+            } )
+            
+        }
+    }
+
+
+    // Collision Joueur / Platforme
+    platforms.forEach(platform => {
+        if (player.position.y + player.height <= platform.position.y 
+            && player.position.y + player.height + player.velocity.y >= platform.position.y 
+            && player.position.x + player.width >= platform.position.x
+            && player.position.x <= platform.position.x + platform.width) {
+            player.velocity.y = 0
+        }
+
+        if (scrollOffset > 2000) {
+            console.log('Win')
+        }
+    })
+
 }
 
 animate()
@@ -79,7 +133,8 @@ addEventListener('keydown', ({keyCode}) => {
             break
         case 90:
             console.log('up')
-            player.velocity.y -= 20
+            if (event.repeat) { return }
+                player.velocity.y -= 15
             break            
     }
 })
@@ -100,7 +155,7 @@ addEventListener('keyup', ({keyCode}) => {
             break
         case 90:
             console.log('up')
-            player.velocity.y -= 20
+            player.velocity.y -= 15
             break            
     }
 })
